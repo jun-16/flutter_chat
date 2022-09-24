ef53e21a39731986a6bff7be44936258b5b78e30
jun-16 <sumitomo.firebase@gmail.com>
2022-09-24 11:04:44 +0900

    コードの整理を実施

---
+++ lib/pages/top_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TopPage extends StatefulWidget {
+  const TopPage({Key? key}) : super(key: key);
+
+  @override
+  State<TopPage> createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('チャットアプリ'),),
+      body: const Center(child: Text('メイン画面です'))
+    );
+  }
+}
