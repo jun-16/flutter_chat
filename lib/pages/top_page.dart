f617813cad540d2c6d3ba36c20516ed8fa2f5942
jun-16 <sumitomo.firebase@gmail.com>
2022-09-24 23:08:39 +0900

    UI作成完了

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/model/user.dart';
+import 'package:flutter_chat/pages/setting_profile_page.dart';
+import 'package:flutter_chat/pages/talk_room_page.dart';
 
 class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
@@ -27,32 +29,51 @@ class _TopPageState extends State<TopPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('チャットアプリ'),),
+      appBar: AppBar(
+        title: const Text('チャットアプリ'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => const SettingProfilePage()
+                ));
+              },
+              icon: const Icon(Icons.settings)
+          )
+        ],
+      ),
       body: ListView.builder(
         itemCount: userList.length,
         itemBuilder: (context, index) {
-          return SizedBox(
-            height: 70,
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: CircleAvatar(
-                    radius: 30,
-                    backgroundImage: userList[index].imagePath == null
-                      ? null
-                      : NetworkImage(userList[index].imagePath!),
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => TalkRoomPage(userList[index].name)
+              ));
+            },
+            child: SizedBox(
+              height: 70,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: userList[index].imagePath == null
+                        ? null
+                        : NetworkImage(userList[index].imagePath!),
+                    ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(userList[index].name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                    Text(userList[index].lastMessage, style: const TextStyle(color: Colors.grey),),
-                  ],
-                )
-              ],
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(userList[index].name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                      Text(userList[index].lastMessage, style: const TextStyle(color: Colors.grey),),
+                    ],
+                  )
+                ],
+              ),
             ),
           );
         }
