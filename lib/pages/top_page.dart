3ae8d894977c721490c4ac5783c41c8ba0f13050
jun-16 <sumitomo.firebase@gmail.com>
2022-09-24 12:01:02 +0900

    トップページのUI作成

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -19,7 +19,7 @@ class _TopPageState extends State<TopPage> {
     User(
       name: '佐藤',
       uid: 'def',
-      imagePath:'https://play-lh.googleusercontent.com/3ImQkiRynf23t3kZ3PMtmGNA0OaozdlzjkH0e2OTV_wmHxUdXglyGpnWuxqmofmMAw=w240-h480-rw',
+      // imagePath:'https://play-lh.googleusercontent.com/3ImQkiRynf23t3kZ3PMtmGNA0OaozdlzjkH0e2OTV_wmHxUdXglyGpnWuxqmofmMAw=w240-h480-rw',
       lastMessage: 'ありがとう'
     )
   ];
@@ -28,7 +28,35 @@ class _TopPageState extends State<TopPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('チャットアプリ'),),
-      body: const Center(child: Text('メイン画面です'))
+      body: ListView.builder(
+        itemCount: userList.length,
+        itemBuilder: (context, index) {
+          return SizedBox(
+            height: 70,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: userList[index].imagePath == null
+                      ? null
+                      : NetworkImage(userList[index].imagePath!),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(userList[index].name, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                    Text(userList[index].lastMessage, style: const TextStyle(color: Colors.grey),),
+                  ],
+                )
+              ],
+            ),
+          );
+        }
+      ),
     );
   }
 }
